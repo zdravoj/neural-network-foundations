03379623fd58f32a07b5191c7fc6d0b32181dd68
Austin Wood <austinwood360@gmail.com>
2024-01-28 02:41:15 -0700

    some makemore module, makemore pt4 up to dbnmean

---
+++ makemore_clone/makemore_v3.py
@@ -0,0 +1,91 @@
+import torch
+
+class Linear:
+  
+  def __init__(self, fan_in, fan_out, bias=True, generator=None):
+    # fan in: n_inputs
+    # fan out: n_outputs
+    # fan_in**0.5 is kaiming initialization
+    self.weight = torch.randn((fan_in, fan_out), generator=generator) / fan_in**0.5
+    # biases are initialized zero
+    self.bias = torch.zeros(fan_out) if bias else None
+  
+
+  def __call__(self, x): # W @ x + b
+    self.out = x @ self.weight
+    if self.bias is not None:
+      self.out += self.bias
+    return self.out
+  
+  # list of parameter tensors
+  def parameters(self):
+    return [self.weight] + ([] if self.bias is None else [self.bias])
+
+
+class BatchNorm1d:
+  
+  def __init__(self, dim, eps=1e-5, momentum=0.1):
+    # epsilon, for increasing entropy / reducing overfitting
+    self.eps = eps
+    # for running mean and variance
+    self.momentum = momentum
+    # for grad tracking in forward / backward pass
+    # false reduces overhead when grad tracking unnecessary
+    self.training = True
+    # parameters (trained with backprop)
+    self.gamma = torch.ones(dim) # stretch, squeeze
+    self.beta = torch.zeros(dim) # shift
+    # buffers (trained with a running 'momentum update')
+    self.running_mean = torch.zeros(dim)
+    self.running_var = torch.ones(dim)
+  
+  def __call__(self, x):
+    # calculate the forward pass
+    if self.training:
+      xmean = x.mean(0, keepdim=True) # batch mean
+      xvar = x.var(0, keepdim=True) # batch variance
+    else: # predictions / forward pass only
+      xmean = self.running_mean # training set mean
+      xvar = self.running_var # training set variance
+    xhat = (x - xmean) / torch.sqrt(xvar + self.eps) # normalize to unit variance
+    # self.out is used mainly for plotting / network statistics
+    self.out = self.gamma * xhat + self.beta
+    # update the buffers
+    if self.training:
+      with torch.no_grad(): # disables computational graph overhead for backward pass
+        self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * xmean
+        self.running_var = (1 - self.momentum) * self.running_var + self.momentum * xvar
+    return self.out
+
+  def parameters(self):
+    return [self.gamma, self.beta]
+
+# self-explanatory
+class Tanh:
+  def __call__(self, x):
+    self.out = torch.tanh(x)
+    return self.out
+  def parameters(self):
+    return []
+
+
+class ExampleModel:
+
+  def __init__(self, n_in, n_out, n_hidden, n_layers, block_size, generator=None):
+    self.C = torch.randn((n_out, n_in), generator=generator)
+    self.layers = [Linear(n_in * block_size, n_hidden), Tanh()]
+    for _ in range(n_layers):
+      self.layers.append(Linear(n_hidden, n_hidden))
+      self.layers.append(Tanh())
+    self.layers.append(n_hidden, n_out)
+  
+    with torch.no_grad():
+      self.layers[-1].gamma *= 0.1
+      for layer in self.layers[:-1]:
+        if isinstance(layer, Linear):
+          layer.weight *= 1.0
+
+    self.parameters = [self.C] + [p for layer in self.layers for p in layer.parameters()]
+    self.parameter_count = sum(p.nelement() for p in self.parameters)
+    for p in self.parameters:
+      p.requires_grad = True
